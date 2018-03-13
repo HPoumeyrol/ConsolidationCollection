677853c3c9b77c61a63585971f79a698da86f582
HPoumeyrol <hugues.poumeyrol.pf1@gmail.com>
2018-03-13 05:35:35 +0100

    final version

--- src/co/simplon/CountWordOccurrence.java
+++ src/co/simplon/CountWordOccurrence.java
@@ -15,12 +15,10 @@ public class CountWordOccurrence {
 	
 	public static void main(String[] args) throws  IOException{
 		String fileName1 = args.length > 0 ? args[0] : "";
-		Integer biggerWordCountFound= 0; 
 		
 		//Filling mapWordsCount with content of first file
-		mapWordsCount= readWordMap(fileName1, biggerWordCountFound);
+		mapWordsCount= readWordMap(fileName1);
 	    
-		System.out.println("Bigger WordCount found is " + biggerWordCountFound);
 		System.out.println("");
 		
 		
@@ -32,8 +30,8 @@ public class CountWordOccurrence {
 	    {
 	    	Integer wordCount= mapWordsCount.get(word);
 	    	System.out.println(word + " : " + wordCount);
-	    	Integer key= biggerWordCountFound - wordCount; //Create a Key with the inverse of wordCount for descending sorting
-	    	String value= wordCount + " : " + word; //inital value = wordCount + first word found
+	    	Integer key= 0 - wordCount; //Create a Key with the inverse of wordCount for descending sorting
+	    	String value= word; //inital value = first word found
 	    	
 	    	if(mapSortOccurences.containsKey(key)) { // if Key already exist in mapSortOccurences
 	    		value = mapSortOccurences.get(key) + ", " + word;  // retrieve value and add word
@@ -45,15 +43,16 @@ public class CountWordOccurrence {
 	    
 	    System.out.println("");
 	    System.out.println("List of words found in descending order of occurrence numbers");
+	    // walk mapSortOccurences and print content : occurences is the absolute value of key
 	    for(Integer key: mapSortOccurences.keySet())
 	    {
-	    	System.out.println(key + " :: " + mapSortOccurences.get(key));
+	    	System.out.println(Math.abs(key) + " :: " + mapSortOccurences.get(key));
 	    }
 	    
 	}
 
 	
-	private static TreeMap<String, Integer> readWordMap(String fileName, Integer biggerWordCountFound) throws IOException {
+	private static TreeMap<String, Integer> readWordMap(String fileName) throws IOException {
 		Path filePath = Paths.get(fileName);
 		TreeMap<String, Integer> res= new TreeMap<String, Integer>();
 		
@@ -65,12 +64,8 @@ public class CountWordOccurrence {
 				if(res.containsKey(word)) {	wordCount= res.get(word); }
 				// Store word with new wordCount
 				res.put(word, ++wordCount);
-				
-				
-				if(wordCount > biggerWordCountFound) { biggerWordCountFound= wordCount; }
-			}
+							}
 		}
-		System.out.println("bigger wc = " + biggerWordCountFound);
 		return res;
 	}
 
